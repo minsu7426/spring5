728510fb7254959918c1832de771ad12042ca9c6
minsu7426 <minsu7426@naver.com>
2022-06-16 17:58:29 +0900

    0616 17:58

---
+++ sp6-chap04/src/test/java/sp6/sp6_chap03/AppTest.java
@@ -0,0 +1,20 @@
+package sp6.sp6_chap03;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
